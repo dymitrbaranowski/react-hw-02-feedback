298f465eb897806e17f7486558b6c9421c203ebe
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-12-29 20:30:26 +0200

    fox1

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 
-export class App extends Component {
+class App extends Component {
   state = {
     good: 0,
     neutral: 0,
